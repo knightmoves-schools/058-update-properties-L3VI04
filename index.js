81e8b59b07d6a1bc021726c8a38752428c595696
L3VI04 <ljenkins@anamosa.k12.ia.us>
2025-11-04 13:19:34 -0600

    Add person object and birthday celebration function

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+// Create a person object with age 0
+const person = {
+  age: 0
+};
+
+// Function to increment person's age by one year
+function celebrateBirthday(person) {
+  person.age += 1;
+  return person;
+}
+
 
